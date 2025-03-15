b18cb09735805735e13436920acbf87386b7f03d
Cosmodude <vladislav.lenskii@gmail.com>
2025-03-15 18:39:09 +0900

    pool creation functions written and tested

---
+++ migrations/initPool.ts
@@ -0,0 +1,38 @@
+import * as anchor from "@project-serum/anchor";
+import { Trepa } from "../target/types/trepa";
+import { createPool } from "./utils/createPool";
+import { ConnectedSolanaWallet } from "@privy-io/react-auth";
+
+async function main() {
+  // Set up the provider and program
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+  const program = anchor.workspace.Trepa as anchor.Program<Trepa>;
+
+  if (!program) {
+    throw new Error("⚠️ Program not found. Make sure it is deployed.");
+  }
+
+  console.log(`📜 Program loaded with ID: ${program.programId.toBase58()}`);
+
+
+  const question = "b9cdc74e-c59a-4dbc-8006-c3e32604081a"; // 16 bytes uuid
+  // Calculate prediction end time one year later (in seconds)
+  const predictionEndTime = Math.floor(Date.now() / 1000) + 31536000;
+  // Prepare transaction to initialize the Config account
+  const tx = await createPool(
+    program, 
+    provider.wallet as unknown as ConnectedSolanaWallet, 
+    question, 
+    predictionEndTime
+  );
+
+  const signature = await provider.sendAndConfirm(tx);
+  console.log("Transaction Signature:", signature);
+}
+
+main()
+  .then(() => console.log("Initialization successful"))
+  .catch((err) => {
+    console.error("Error initializing:", err);
+  });
---
+++ migrations/utils/createPool.ts
@@ -0,0 +1,45 @@
+import { BN, Program } from "@project-serum/anchor";
+import { PublicKey, SystemProgram, SYSVAR_CLOCK_PUBKEY, Transaction } from "@solana/web3.js";
+import { ConnectedSolanaWallet } from '@privy-io/react-auth';
+import { Trepa } from "../../target/types/trepa";
+
+/**
+ * Creates a new pool with the given question and prediction end time.
+ * @param program - The program instance.
+ * @param question - The question to be asked.
+ * @param predictionEndTime - The timestamp when the prediction ends.
+ */
+export async function createPool(
+    program: Program<Trepa>, 
+    wallet: ConnectedSolanaWallet, 
+    questionId: string, 
+    predictionEndTime: number
+): Promise<Transaction> {
+    const cleanedQuestionId = questionId.replace(/-/g, '');
+    const questionBytes = Buffer.from(cleanedQuestionId, 'hex');
+    const questionArray = Array.from(questionBytes);
+    if (questionArray.length !== 16) {
+        throw new Error(`Question must be 16 bytes, not ${questionArray.length} bytes for string ${questionBytes}`);
+    }
+    console.log("Program ID:", program.programId.toBase58());
+
+    // Get the PDA for the pool
+    const [poolPDA] = await PublicKey.findProgramAddressSync(
+        [Buffer.from("pool"), questionBytes],
+        program.programId
+    );
+
+    // Create the pool
+    const tx = await program.methods
+        .createPool(questionArray, new BN(predictionEndTime))
+        .accounts({
+            pool: poolPDA,
+            admin: wallet.address,
+            systemProgram: SystemProgram.programId,
+            clock: SYSVAR_CLOCK_PUBKEY,
+        })
+        .transaction();
+
+    console.log(`Transaction created! ${tx}`);
+    return tx;
+}
