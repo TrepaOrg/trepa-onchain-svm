538887d232910aca7c76280c974b51fbea6394d2
Cosmodude <vladislav.lenskii@gmail.com>
2025-03-16 15:31:25 +0900

    resolve pool script

---
+++ migrations/resolvePool.ts
@@ -0,0 +1,39 @@
+import * as anchor from "@project-serum/anchor";
+import { Trepa } from "../target/types/trepa";
+import { resolvePool } from "./utils/resolvePool";
+
+async function main() {
+  // Set up the provider and program
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+  const program = anchor.workspace.Trepa as anchor.Program<Trepa>;
+
+  if (!program) {
+    throw new Error("⚠️ Program not found. Make sure it is deployed.");
+  }
+
+  console.log(`📜 Program loaded with ID: ${program.programId.toBase58()}`);
+
+
+  const question = "b9cdc74e-c59a-4dbc-8006-c3e326040813"; // 16 bytes uuid
+  // Calculate prediction end time one year later (in seconds)
+  // Prepare transaction to initialize the Config account
+  const tx = await resolvePool(
+    program, 
+    question, 
+    provider.wallet.publicKey, 
+    [1000000],
+    [provider.wallet.publicKey]
+  );
+
+  console.log("Pool PDA:", tx.instructions[0].keys[1].pubkey.toBase58());
+
+  const signature = await provider.sendAndConfirm(tx);
+  console.log("Transaction Signature:", signature);
+}
+
+main()
+  .then(() => console.log("Resolved pool successfully"))
+  .catch((err) => {
+    console.error("Error resolving pool:", err);
+  });
---
+++ migrations/utils/resolvePool.ts
@@ -0,0 +1,62 @@
+import { BN, Program } from "@project-serum/anchor";
+import { PublicKey, SystemProgram, SYSVAR_CLOCK_PUBKEY, Transaction } from "@solana/web3.js";
+import { Trepa } from "../../target/types/trepa";
+
+/**
+ * Resolves a pool with the given question and prediction end time.
+ * @param program - The program instance.
+ * @param poolId - The pool to be resolved. (same as the question id)
+ * @param admin - The admin account.    
+ * @param prizes - The prizes to be distributed.
+ * @param predictors - The predictors to get prizes.
+ */
+export async function resolvePool(
+    program: Program<Trepa>, 
+    poolId: string, 
+    admin: PublicKey,
+    prizes: number[],
+    predictors: PublicKey[]
+): Promise<Transaction> {
+    console.log("Program ID:", program.programId.toBase58());
+
+    // Get the PDA for the pool
+    const cleanedPoolId = poolId.replace(/-/g, '');
+    const poolBytes = Buffer.from(cleanedPoolId, 'hex');
+    const [poolPDA] = await PublicKey.findProgramAddressSync(
+        [Buffer.from("pool"), poolBytes],
+        program.programId
+    );
+    console.log("Pool PDA:", poolPDA.toBase58());
+    
+    // Get the PDA for the predictions
+    const predictionPDA = predictors.map(predictor => {
+        const [predictionPDA] = PublicKey.findProgramAddressSync(
+            [Buffer.from("prediction"), poolBytes, predictor.toBuffer()],
+            program.programId
+        );
+        return {
+            pubkey: predictionPDA,
+            isWritable: true,
+            isSigner: false,
+        }
+    });
+
+    const prizeAmounts = prizes.map(prize => new BN(prize)); // 0.001 SOL
+
+    if (prizeAmounts.length !== predictionPDA.length) {
+        throw new Error("Mismatched prize count");
+    }
+
+    // Resolve the pool
+    const tx = await program.methods
+        .resolvePool(prizeAmounts)
+        .accounts({
+            pool: poolPDA,
+            admin
+        })
+        .remainingAccounts(predictionPDA)
+        .transaction();
+
+    console.log(`Transaction created! ${tx}`);
+    return tx;
+}
